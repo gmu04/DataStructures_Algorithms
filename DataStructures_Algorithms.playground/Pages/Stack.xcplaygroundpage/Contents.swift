8f8e487b4224afa7a3ee67d911679a93f2d70d7f
GokhanMutlu <gokhanmutlu04@yahoo.com>
2021-09-07 09:38:17 +0300

    Stack data structure

---
+++ DataStructures_Algorithms.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//LIFO - last in first out
+
+struct Stack<T>{
+	private(set) var data = [T]()
+	var count:Int { data.count }
+	
+	mutating func push(_ value:T){
+		data.insert(value, at: 0)
+	}
+	
+	mutating func pop() -> T?{
+		defer {
+			if data.indices.contains(0){ data.remove(at: 0) }
+		}
+		return data.first
+	}
+}
+
+var stack = Stack<Int>()
+stack.push(1);stack.push(22);stack.push(11);stack.push(37); print(stack.data, ", item count: \(stack.count)")
+print(stack.pop(), stack.data)
+print(stack.pop(), stack.data)
+print(stack.pop(), stack.data)
+print(stack.pop(), stack.data)
+print(stack.pop(), stack.data)
+
+
+//: [Next](@next)
